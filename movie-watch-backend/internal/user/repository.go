4e2ad670bece8a77366fbcebbd0f18008922f062
Beeram Pranith Reddy <pranithreddy1316@gmail.com>
2025-08-08 22:34:53 +0530

    Fix schema details Changed variable type

--- movie-watch-backend/internal/user/repository.go
+++ movie-watch-backend/internal/user/repository.go
@@ -2,16 +2,17 @@ package user
 
 import (
 	"context"
+	"movie-watch-backend/internal/models"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User interface {
-	CreateUser(ctx context.Context, user *User) error
-	GetUserById(ctx context.Context, id primitive.ObjectID) (*User, error)
-	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	GetUserByGoogleId(ctx context.Context, googleId string) (*User, error)
-	UpdateUser(ctx context.Context, user *User) error
-	DeleteUser(ctx context.Context, user *User) error
+	CreateUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id primitive.ObjectID) (*models.User, error)
+	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	GetUserByGoogleId(ctx context.Context, googleId string) (*models.User, error)
+	UpdateUser(ctx context.Context, user *models.User) error
+	DeleteUser(ctx context.Context, user *models.User) error
 	ExistsByEmail(ctx context.Context, email string) (bool, error)
 }
